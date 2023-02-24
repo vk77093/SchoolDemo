8a92d28cb6cc84a9c6d76e3d19e507ae6cdcd2be
Team ROSPL <vkdevrospl@hotmail.com>
2023-02-24 11:08:10 +0530

    PushData

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\StudentManagement\FeeCategory;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +23,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin@123'),
+            'userType'=>'Aartdmin',
+        ]);
+        $data=[[
+            'fee_cate_name'=>'Registration Fee',
+        ],
+        ['fee_cate_name'=>'Montly Fees'],
+        ['fee_cate_name'=>'Exam Fees'],
+        
+    ];
+    FeeCategory::insert($data);
+        
     }
 }
